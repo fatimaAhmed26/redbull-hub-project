2f11889c41c2166ffed6e97ab63adb8e550fff1f
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-18 02:13:39 +0300

    creting the files ,(MVC structur)

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const app = express();
-
+const path= require('path')
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
@@ -30,6 +30,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
+ app.use(express.static(path.join(__dirname, "public")));
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false,
