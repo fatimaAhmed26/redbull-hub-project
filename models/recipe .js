4c20bdd966514adf48c76b4dc54fca2880d321a6
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-19 09:49:53 +0300

    creating the comments schema

--- models/recipe .js
+++ models/recipe .js
@@ -1,4 +1,21 @@
 const mongoose = require('mongoose')
+commentSchema =new mongoose.Schema({
+    comment:{
+        type:String,
+        required: true,
+
+    },
+    author:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        
+},
+recipeId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'Recipe'
+},
+
+})
 const recipeSchema = new mongoose.Schema({
 description :{
     type:String,
@@ -18,6 +35,8 @@ userId:{
         ref:'User',
         
 },
+comments :[commentSchema],
+
 },{timestamps:true})
 const Recipe = mongoose.model('Recipe',recipeSchema)
 module.exports = Recipe
