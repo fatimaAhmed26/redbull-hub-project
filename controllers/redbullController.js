dbfd2698a68434b1154d3463613d447b6166e908
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-19 09:12:16 +0300

    enabling cloudinary and multer

---
+++ controllers/redbullController.js
@@ -0,0 +1,33 @@
+// enabling images 
+const cloudinary = require('../config/cloudinary')
+const uploadImage = (fileBuffer) => {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.uploader.upload_stream(
+      {
+        folder: 'redbull-hub/redbull',
+        resource_type: 'image',
+      },
+      (error, result) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(result)
+        }
+      }
+    )
+
+    uploadStream.end(fileBuffer)
+  })
+}
+const uploadedImage = await uploadImage(req.file.buffer)
+    console.log(uploadedImage)
+    const listingData={}
+    listingData.price =req.body.price
+    listingData.streetAddress=req.body.streetAddress
+    listingData.city=req.body.city
+    listingData.size=req.body.size
+    listingData.owner= req.session.user._id
+    listingData.image={
+       url: uploadedImage.secure_url,
+    publicId: uploadedImage.public_id,
+    }
