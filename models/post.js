de3dce00122ea9010b7c50a7f8febfb782b05b2a
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-07-22 11:31:35 +0300

    adding the posts modle

---
+++ models/post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+postSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      url: {
+        type: String,
+        required: true,
+      },
+      publicId: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  { timestamps: true },
+);
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
